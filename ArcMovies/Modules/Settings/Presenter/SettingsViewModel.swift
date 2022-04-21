231acc32f9e51e9ecbe5d0ece661ff077587ecc6
Yuri Frota <yurisaboiaf@gmail.com>
2022-04-21 19:42:22 -0300

    added pin registration upon guard enabled

--- ArcMovies/Modules/Settings/Presenter/SettingsViewModel.swift
+++ ArcMovies/Modules/Settings/Presenter/SettingsViewModel.swift
@@ -14,6 +14,7 @@ class SettingsViewModel: ObservableObject {
     @Published var guardEnabled = false {
         didSet {
             UserDefaults.standard.set(guardEnabled, forKey: UserDefaults.authenticationEnabledKey)
+            if guardEnabled { registerNewPin() }
         }
     }
 
